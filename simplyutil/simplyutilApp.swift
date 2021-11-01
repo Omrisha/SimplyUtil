7466a04aa4eba585ca7eda83b3ea181f1478c12e
Omri Shapira <omris11@mail.afeka.ac.il>
2021-11-01 14:56:42 +0200

    Initial Commit

---
+++ simplyutil/simplyutilApp.swift
@@ -0,0 +1,17 @@
+//
+//  simplyutilApp.swift
+//  simplyutil
+//
+//  Created by Omri Shapira on 01/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct simplyutilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
